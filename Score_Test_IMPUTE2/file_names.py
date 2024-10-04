f3382b53aa172e673883be0544b66d8ecbc0029d
Daniel Barnes <48189102+drb54@users.noreply.github.com>
2024-10-04 12:35:05 +0100

    Delete Score_Test_IMPUTE2 directory

--- Score_Test_IMPUTE2/file_names.py
+++
@@ -1,6 +0,0 @@
-pheno_file_name   = '/lucy/vol2/genepi/andrewl/IMPUTE2/DATA/TERT_icogs.sample'
-kinship_file_name = '/lucy/vol2/genepi/andrewl/IMPUTE2/DATA/kinship.brca1.icogs.forScore.dat'
-geno_file_name    = '/lucy/vol2/genepi/andrewl/IMPUTE2/DATA/tert_imputation_results.txt_renamed_duplicates'
-info_file_name    = '/lucy/vol2/genepi/andrewl/IMPUTE2/DATA/tert_imputation_results.txt_info_renamed_duplicates'
-results_file_name = '/lucy/vol2/genepi/andrewl/IMPUTE2/RESULTS/tert_py_results.csv'
-ovarian = False
